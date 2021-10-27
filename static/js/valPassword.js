8226cc5e3be1c89163c77ddd9d5344ea8e7dfbf9
sleikung <sleikung@uninorte.edu.co>
2021-10-27 12:47:11 -0500

    Changes js

--- static/js/valPassword.js
+++ static/js/valPassword.js
@@ -1,16 +1,35 @@
-var password = document.formRegistro.password;
+function validar()
+{
+    var nombreUsuario = document.formulariouser_profile.usuario;
+    var claveInput = document.formulariouser_profile.clave;
+    //var correoInput = document.formularioRegistro.correo;
 
-var passid_len = password.value.length;
-  if (passid_len == 0 || passid_len < 8) {
-    alert("Debes ingresar una password con mas de 8 caracteres");
-    passid.focus();
-  }
+    /*var formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;*/
 
-function mostrarPassword(obj) {
-  var obj = document.getElementById("password");
-  obj.type = "text";
-}
-function ocultarPassword(obj) {
-  var obj = document.getElementById("password");
-  obj.type = "password";
+    var swErrores=false;
+
+    console.log(nombreUsuario.value + "-"+claveInput.value);
+
+    if(nombreUsuario.value.length == 0 || nombreUsuario.value.length < 8)
+    {
+        document.getElementById("errorusuario").innerHTML="El nombre de usuario debe tener mínimo 8 caracteres.";
+        nombreUsuario.focus();
+        swErrores=true;
+    }
+
+    if(claveInput.value.length == 0 || claveInput.value.length < 8)
+    {
+        document.getElementById("errorClave").innerHTML="La clave debe tener mínimo 8 caracteres.";
+        claveInput.focus();
+        swErrores=true;
+    }
+
+    if( swErrores==true)
+    {
+        return false;
+    }
+    else{
+        return true;
+    }
+
 }
